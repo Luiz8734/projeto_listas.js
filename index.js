18d2cf217e555d965a19da5ca8bd2f27b7ab3026
Luiz8734 <luizmoraissantos17@gmail.com>
2025-07-23 21:11:02 -0300

    Atualiza instruções de execução no README

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-(use "git add <file>..." to include in what will be committed)
-        README.md
-        img/
-        index.html
-        index.js
-        scr/
-        styles.cssimport { criarItemDaLista } from "./scr/criarItemDaLista.js";
+import { criarItemDaLista } from "./scr/criarItemDaLista.js";
 import verificarListaVazia from "./scr/verificarListaVazia.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
